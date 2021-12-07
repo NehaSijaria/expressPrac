ce2b3856270add6cb7b130cd0d8e34f6b03d9878
Neha <nehasijaria@gmail.com>
2021-12-07 12:53:10 -0500

    create home route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
-const app = express();
 const PORT = 5001;
 
+//Init express
+const app = express();
+
+//endpoints/route handler to homepage/Index page
+app.get('/', (req,res) => { 
+  res.send('<h1>Welcome, homepage in html</h1>') //res in html
+  //res.send('Welcome home page'); // res in txt
+})
+//Server listening
 app.listen(PORT, ()=>{
   console.log(`app listening on localhost:${PORT}`)
 })
