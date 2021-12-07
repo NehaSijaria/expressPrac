6b5127ce2133b3afc1a5f748e1249cb25b1b638b
Neha <nehasijaria@gmail.com>
2021-12-07 13:30:57 -0500

    add txt file and code cleanup

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const path = require('path'); //to deal with filepath
-const PORT = process.env.PORT || 5001;
-//look at envVar (when we deploy we look at env else 5000)
 
 //Init express
 const app = express();
-//set static folder
-app.use(express.static(path.join(__dirname, 'public')))
 
-//endpoints/route handler to homepage/Index page
-// app.get(//Fetch from db,Load pages,return json,full access to eq,res)res=>http.res(render template,send json data, redirect,send data,file)
-app.get('/', (req,res) => { 
-  res.send('<h1>Welcome, homepage in html</h1>') //res in html
-  //res.send('Welcome home page'); // res in txt
-})
 
-// app.get('/about', (req,res) => { 
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// }) //send html file in public>index.html
-//Server listening
 
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, ()=>{
   console.log(`app listening on localhost:${PORT}`)
 })
