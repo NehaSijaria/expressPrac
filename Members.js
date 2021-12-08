ba27029faba234d0f7b497c1af1b9ff78ff9eb99
Neha <nehasijaria@gmail.com>
2021-12-08 00:41:28 -0500

    iterate indiv member

--- Members.js
+++ Members.js
@@ -1,14 +1,16 @@
 const members = [
   {
+    id:1,
     name:"abc",
     email:"a@a.com",
     status:"active"
   },
   {
+    id:2,
     name:"bac",
     email:"b@b.com",
     status:"inactive"
   }
 ]
 
-module.exports = { members }
+module.exports =  members;
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const path = require('path'); //to deal with filepath
-const moment = require('moment') //for current date
-const members = require('./Members')
+const members = require('./Members');
+const logger = require('./middleware/logger');
 //Init express
 const app = express();
 
-const logger = (req, res, next) => {
-  //console.log('hello');
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${moment().format()}`);
-  next();
-}
-
-//Init middleware
+//Init middleware - it will keep running with every request
 app.use(logger);
 
 //Gets all members
 app.get('/api/members', (req,res) => res.json(members)
 )
+//get single member
+app.get('/api/members/:id', (req,res) => {
+  //res.send(req.params.id) return id;
+  //filter indiv member based on id
+  //anything returned as "/:id" is string while member.id is a number
+  res.json(members.filter(member => member.id === parseInt(req.params.id)))
+})
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
