7fc998055b50e8476ee9bfc372aa123340f7e647
Neha <nehasijaria@gmail.com>
2021-12-07 22:47:05 -0500

    separated Member file

---
+++ Members.js
@@ -0,0 +1,14 @@
+const members = [
+  {
+    name:"abc",
+    email:"a@a.com",
+    status:"active"
+  },
+  {
+    name:"bac",
+    email:"b@b.com",
+    status:"inactive"
+  }
+]
+
+module.exports = { members }
