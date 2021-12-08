6003183ed85097bfdc66bb0c89e89d0cf33af99c
Neha <nehasijaria@gmail.com>
2021-12-08 00:50:48 -0500

    chk condition to iterate member

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ app.get('/api/members', (req,res) => res.json(members)
 )
 //get single member
 app.get('/api/members/:id', (req,res) => {
+  //return boolean
+  const found = members.some(member => member.id === parseInt(req.params.id))
   //res.send(req.params.id) return id;
   //filter indiv member based on id
   //anything returned as "/:id" is string while member.id is a number
+  if(found){
   res.json(members.filter(member => member.id === parseInt(req.params.id)))
+} else {
+  res.status(400).json({msg:`member with id:${req.params.id} not found`})
+}
 })
 
 //set static folder
